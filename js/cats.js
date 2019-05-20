63a42b3644399b014009be04427038119af8b336
BartBros <bb.aambroziak@gmail.com>
2019-05-20 12:09:34 +0200

    responsive, simple layout, links to img

---
+++ js/cats.js
@@ -0,0 +1,54 @@
+const win = $(window);
+const doc = $(document);
+const url = 'https://aws.random.cat/meow';
+const urlDog = 'https://random.dog/woof.json';
+
+function newCat() {
+    $.getJSON(url, function(data) {
+        $(data).each(function (index, prop) {
+            // Create main img Div
+            let newDiv = document.createElement('div');
+            $(newDiv).addClass('main d-flex flex-column align-items-center mb-5');
+            // Create IMG
+            let newImage = document.createElement('img'); 
+            $(newImage).addClass('img-fluid');
+            $(newImage).attr({
+                src: prop.file,
+                title: "Cat"
+              });
+            // Create Div for links
+            let divLinks = document.createElement('div'); 
+            $(divLinks).addClass('div-links d-flex w-100');
+            // Create link for download
+            let aDownload = document.createElement('a');
+            $(aDownload).attr('href', prop.file); 
+            $(aDownload).addClass('cat-link');
+            $(aDownload).html('<i class="fas fa-download"></i> Download');
+            // Create link for share on FB
+            let aFbShare = document.createElement('a');
+            $(aFbShare).attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + prop.file); 
+            $(aFbShare).addClass('cat-link');
+            $(aFbShare).html('<i class="fab fa-facebook-square"></i> Share on FB');
+
+            $(divLinks).append(aDownload).append(aFbShare);
+
+            $('.content').append(newDiv);
+            $(newDiv).append(newImage).append(divLinks);
+        }); // End Each
+    }); // End getJSON
+};
+
+for (let i = 0; i<10; i++) {
+    newCat();
+};
+
+$(win).on('scroll', () =>  {
+    if (doc.height() - win.height() == win.scrollTop()) {
+        for (let i = 0; i < 3; i++) {
+            newCat();
+        }
+    }
+});
+
+
+    
