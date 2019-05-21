2e600b06323d3d30ff8575fe61960fd0a4c3f5c1
BartBros <bb.aambroziak@gmail.com>
2019-05-21 19:44:33 +0200

    better layout

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function newImageCategory() {
         format: "json",
         tags: category
     };
-    $.getJSON(urlFlickr, optionsCategory, function(data) {
+    $.getJSON(urlFlickr, optionsCategory, function (data) {
         $(data.items).each(function (index, val) {
             // ------------------
             // Create MAIN img Div
@@ -21,11 +21,11 @@ function newImageCategory() {
             // ------------------
             // Create heading Div
             // ------------------
-            let divHead = document.createElement('div'); 
+            let divHead = document.createElement('div');
             $(divHead).addClass('div-head d-flex justify-content-between w-100');
             // Create Author Span
             let headSpan = document.createElement('span');
-            let a1 = val.author.split('"'); 
+            let a1 = val.author.split('"');
             $(headSpan).html('<i class="fas fa-user"></i> ' + a1[1]);
             // Create date Span
             let dateSpan = document.createElement('span');
@@ -41,26 +41,26 @@ function newImageCategory() {
                 href: val.link
             });
             // Create IMG
-            let newImage = document.createElement('img'); 
+            let newImage = document.createElement('img');
             $(newImage).addClass('flickr-image img-fluid');
             $(newImage).attr({
                 src: val.media.m,
                 title: val.title
-              });
+            });
             $(newImageLink).append(newImage);
             // ------------------
             // Create Div for links
             // ------------------
-            let divLinks = document.createElement('div'); 
+            let divLinks = document.createElement('div');
             $(divLinks).addClass('div-links d-flex w-100');
             // Create link for download
             let aDownload = document.createElement('a');
-            $(aDownload).attr('href', val.media.m); 
+            $(aDownload).attr('href', val.media.m);
             $(aDownload).addClass('animal-link');
             $(aDownload).html('<i class="fas fa-download"></i> Download');
             // Create link for share on FB
             let aFbShare = document.createElement('a');
-            $(aFbShare).attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + val.link); 
+            $(aFbShare).attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + val.link);
             $(aFbShare).addClass('animal-link');
             $(aFbShare).html('<i class="fab fa-facebook-square"></i> Share on FB');
             $(divLinks).append(aDownload).append(aFbShare);
@@ -75,17 +75,19 @@ newImageCategory();
 // ------------------
 // NAVIGATION
 // ------------------
-$('nav').on ('click', function(event) {
+$('nav').on('click', function (event) {
     if ($(event.target).attr('id') == 'meme' || $(event.target).attr('id') == 'quote') {
         category = $(event.target).attr('id');
+        $('.active').removeClass('active');
+        event.target.parentNode.classList.add("active");
         newImageCategory();
     }
 });
 
 // Search
-$("#search-form").submit(function(event) {
+$("#search-form").submit(function (event) {
     let searchField = document.querySelector('#search-field');
-    let keyword = $('#search-field').val(); 
+    let keyword = $('#search-field').val();
     event.preventDefault();
     category = keyword;
     newImageCategory();
@@ -94,9 +96,9 @@ $("#search-form").submit(function(event) {
 // Search Check
 function checkSearch() {
     let keyword = $('#search-field').val();
-	const searchInput = document.querySelector("#search-field");
-	
-	if (blacklist.includes(keyword)) {
-		searchInput.setCustomValidity("This keyword is blacklisted!");
-	} 
+    const searchInput = document.querySelector("#search-field");
+
+    if (blacklist.includes(keyword)) {
+        searchInput.setCustomValidity("This keyword is blacklisted!");
+    }
 }
--- js/dogs.js
+++ js/dogs.js
@@ -4,30 +4,30 @@ const url = 'https://random.dog/woof.json';
 let scrollEnabled = true;
 
 function newDog() {
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function (data) {
         $(data).each(function (index, prop) {
             if (prop.url.slice(-4) !== '.mp4' && prop.url.slice(-5) !== '.webm') {
                 // Create main img Div
                 let newDiv = document.createElement('div');
                 $(newDiv).addClass('main d-flex flex-column align-items-center mb-5');
                 // Create IMG
-                let newImage = document.createElement('img'); 
+                let newImage = document.createElement('img');
                 $(newImage).addClass('img-fluid');
                 $(newImage).attr({
                     src: prop.url,
                     title: "Dog"
                 });
                 // Create Div for links
-                let divLinks = document.createElement('div'); 
+                let divLinks = document.createElement('div');
                 $(divLinks).addClass('div-links d-flex w-100');
                 // Create link for download
                 let aDownload = document.createElement('a');
-                $(aDownload).attr('href', prop.url); 
+                $(aDownload).attr('href', prop.url);
                 $(aDownload).addClass('animal-link');
                 $(aDownload).html('<i class="fas fa-download"></i> Download');
                 // Create link for share on FB
                 let aFbShare = document.createElement('a');
-                $(aFbShare).attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + prop.url); 
+                $(aFbShare).attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + prop.url);
                 $(aFbShare).addClass('animal-link');
                 $(aFbShare).html('<i class="fab fa-facebook-square"></i> Share on FB');
 
@@ -40,11 +40,11 @@ function newDog() {
     }); // End getJSON
 };
 
-for (let i = 0; i<10; i++) {
+for (let i = 0; i < 10; i++) {
     newDog();
 };
 
-$(win).on('scroll', () =>  {
+$(win).on('scroll', () => {
     if (doc.height() - win.height() == win.scrollTop()) {
         if (scrollEnabled) {
             for (let i = 0; i < 3; i++) {
@@ -55,4 +55,4 @@ $(win).on('scroll', () =>  {
 });
 
 
-    
+
